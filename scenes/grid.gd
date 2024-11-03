488ea6a926a0452ceec93bbedcc7fa72d1e24889
Tobias Bocanegra <tripod@adobe.com>
2024-11-04 00:28:54 +0100

    triangulation

--- scenes/grid.gd
+++ scenes/grid.gd
@@ -1,15 +1,16 @@
 @tool
 extends Polygon2D
 
+@export var rebuild:bool = false:
+  set(value):
+    generate_mesh()
+
 @export var grid_size:int = 32:
   set(value):
     grid_size = value
     generate_mesh()
 
 var meshInstance: MeshInstance2D
-
-#func _ready():
-  #generate_mesh()
 
 func generate_mesh():
   meshInstance = get_node_or_null('mesh')
@@ -24,43 +25,69 @@ func generate_mesh():
   var indices = PackedInt32Array()
 
   #get BB of polygon
-  #var x0:int = 0
-  #var x1:int = 0
-  #var y0:int = 0
-  #var y1:int = 0
-  #for v:Vector2 in polygon:
-    #bb = bb.expand(v)
-  ## snap bb to grid
-  #prints('before', bb)
-  #bb = bb.grow_individual(bb.position.x % grid_size, bb.position.y % grid_size, grid_size - bb.end.x % grid_size, grid_size - bb.end.y % grid_size)
-  #prints('snapped', bb)
+  var x0:int = INF
+  var x1:int = -INF
+  var y0:int = INF
+  var y1:int = -INF
+  for v:Vector2 in polygon:
+    x0 = min(x0, int(v.x) % grid_size)
+    y0 = min(y0, int(v.y) % grid_size)
+    x1 = max(x0, int(v.x) % grid_size + grid_size)
+    y1 = max(y0, int(v.y) % grid_size + grid_size)
 
-  var wx := 20
-  var wy := 20
+  # walk around the polygon in grid_size steps
+  var i:int = 0
+  var dist:float = 0
+  var dest:PackedVector2Array = PackedVector2Array()
+  while i < len(polygon):
+    var v0:Vector2 = polygon[i]
+    var v1:Vector2 = polygon[(i+1) % len(polygon)]
+    while v0 != v1:
+      dest.append(v0)
+      v0 = v0.move_toward(v1, grid_size)
+    i += 1
+  $poly.polygon = dest
+  var tri:PackedInt32Array = Geometry2D.triangulate_polygon(dest)
+  prints('triangluation:', dest.size(), tri.size())
   var idx = 0
-  for y in range(0, wy):
-    for x in range(0, wx):
-      # 0 -- 3
-      # |    |
-      # 1 -- 2
-      var v0 := Vector2(x * grid_size, y * grid_size)
-      verts.append(Vector2((x + 0) * grid_size, (y + 0) * grid_size)) # v0
-      verts.append(Vector2((x + 0) * grid_size, (y + 1) * grid_size)) # v1
-      verts.append(Vector2((x + 1) * grid_size, (y + 1) * grid_size)) # v2
-      verts.append(Vector2((x + 1) * grid_size, (y + 0) * grid_size)) # v3
-      uvs.append(Vector2(0, 0))
-      uvs.append(Vector2(0, 1))
-      uvs.append(Vector2(1, 1))
-      uvs.append(Vector2(1, 0))
-      # top left triangle  0 1 3
-      indices.append(idx)
-      indices.append(idx + 1)
-      indices.append(idx + 3)
-      # bottom right triangle 1 2 3
-      indices.append(idx + 1)
-      indices.append(idx + 2)
-      indices.append(idx + 3)
-      idx += 4
+  for j in range(0, len(tri), 3):
+    verts.append(dest[tri[j]])
+    verts.append(dest[tri[j + 1]])
+    verts.append(dest[tri[j + 2]])
+    uvs.append(Vector2(0, 0))
+    uvs.append(Vector2(0, 1))
+    uvs.append(Vector2(1, 1))
+    indices.append(j)
+    indices.append(j + 1)
+    indices.append(j + 2)
+
+
+  #var wx := 20
+  #var wy := 20
+  #var idx = 0
+  #for y in range(0, wy):
+    #for x in range(0, wx):
+      ## 0 -- 3
+      ## |    |
+      ## 1 -- 2
+      #var v0 := Vector2(x * grid_size, y * grid_size)
+      #verts.append(Vector2((x + 0) * grid_size, (y + 0) * grid_size)) # v0
+      #verts.append(Vector2((x + 0) * grid_size, (y + 1) * grid_size)) # v1
+      #verts.append(Vector2((x + 1) * grid_size, (y + 1) * grid_size)) # v2
+      #verts.append(Vector2((x + 1) * grid_size, (y + 0) * grid_size)) # v3
+      #uvs.append(Vector2(0, 0))
+      #uvs.append(Vector2(0, 1))
+      #uvs.append(Vector2(1, 1))
+      #uvs.append(Vector2(1, 0))
+      ## top left triangle  0 1 3
+      #indices.append(idx)
+      #indices.append(idx + 1)
+      #indices.append(idx + 3)
+      ## bottom right triangle 1 2 3
+      #indices.append(idx + 1)
+      #indices.append(idx + 2)
+      #indices.append(idx + 3)
+      #idx += 4
 
   # Assign arrays to surface array.
   surface_array[Mesh.ARRAY_VERTEX] = verts
@@ -71,4 +98,4 @@ func generate_mesh():
   # Create mesh surface from mesh array.
   # No blendshapes, lods, or compression used.
   meshInstance.mesh = ArrayMesh.new()
-  meshInstance.mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, surface_array)
+  meshInstance.mesh.add_surface_from_arrays(Mesh.PRIMITIVE_LINES, surface_array)
