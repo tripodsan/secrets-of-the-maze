d24b2edf526790b80cd62cbe391e7e4f87a320d3
Lars Krapf <lkrapf@adobe.com>
2024-11-05 14:39:27 +0100

    remove collision body

--- scenes/laser_cast.gd
+++ scenes/laser_cast.gd
@@ -1,14 +1,9 @@
 extends RayCast2D
 
 
-var is_casting := true 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-  pass # Replace with function body.
+var is_casting := false 
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
   pass
 
