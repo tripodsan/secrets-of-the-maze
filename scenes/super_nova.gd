a1825979caaea0805b8488fc739b621317c02f18
Tobias Bocanegra <tripod@adobe.com>
2024-11-10 00:26:05 +0100

    own scene

--- scenes/super_nova.gd
+++ scenes/super_nova.gd
@@ -1,14 +1,12 @@
 extends ColorRect
 
-var _chromatic_distortion := 0.0
-
 func _ready() -> void:
-  visible = false 
+  visible = false
 
 func _input(event) -> void:
   if Input.is_key_pressed(KEY_B):
     _on_supernova()
-    
+
 func _on_supernova():
   var tween := get_tree().create_tween().bind_node(self).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
   tween.tween_method(_on_supernova_update, 0.0, 1.0, 2.0)
