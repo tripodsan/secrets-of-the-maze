357345d6951f7666eb8e7db518f1cf863d438d29
Tobias Bocanegra <tripod@adobe.com>
2024-11-16 18:39:49 +0100

    better fonts

---
+++ scenes/level_map/level_map.gd
@@ -0,0 +1,15 @@
+class_name LevelMap
+extends Node2D
+
+@onready var cursor: AnimatedSprite2D = $cursor
+
+@export var selected_level:LevelMapNode
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+  pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+  pass
