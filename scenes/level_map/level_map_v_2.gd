9ca3ef8b121321a3c63a32144e3ffb2f4de15694
Tobias Bocanegra <tripod@adobe.com>
2024-12-04 00:08:43 +0100

    better level select, maybe

--- scenes/level_map/level_map_v_2.gd
+++ scenes/level_map/level_map_v_2.gd
@@ -95,6 +95,17 @@ const MAX_LEVELS = Vector2i(3, 3)
 var selected:Vector2i = Vector2.ZERO
 
 var selected_level:GDLayer
+var selected_node:LevelNode
+
+class LevelNode extends RefCounted:
+  var pos:Vector2
+  var lay:Vector2i
+
+  func _init(pos:Vector2, lay:Vector2i):
+    self.pos = pos
+    self.lay = lay
+
+var level_nodes:Array[LevelNode] = []
 
 func _ready()->void:
   if Engine.is_editor_hint():
@@ -109,43 +120,53 @@ func _get_minimum_size():
   return Vector2(100, 100)
 
 func _input(_event: InputEvent) -> void:
-  var sign = 1 if selected.y == 0 else -1
+  #
+  # 225\ /-45 / 315
+  #     X
+  # 135/ \45
   if Input.is_action_just_pressed('ui_left'):
-    select(selected + Vector2i.LEFT * sign)
+    select(Vector2i(91, 269))
+    #select(Vector2i(135, 225))
   elif Input.is_action_just_pressed('ui_right'):
-    select(selected + Vector2i.RIGHT * sign)
+    select(Vector2i(-91, 89))
+    #select(Vector2i(-45, 45))
   elif Input.is_action_just_pressed('ui_up'):
-    select((selected + Vector2i.UP * sign + MAX_LEVELS) % MAX_LEVELS)
+    select(Vector2i(181, 359))
+    #select(Vector2i(225, 315))
   elif Input.is_action_just_pressed('ui_down'):
-    select((selected + Vector2i.DOWN * sign + MAX_LEVELS) % MAX_LEVELS)
+    select(Vector2i(1, 179))
+    #select(Vector2i(45, 135))
   elif Input.is_action_just_pressed('ui_accept'):
     SoundController.play_sfx('ui_select')
     level_accepted.emit(selected_level)
 
-func select(next:Vector2i):
-  if next.x < 0: return
-  if next.x > 2:
-    if GameData.get_level(3).unlocked:
-      next = Vector2i(3, 0)
-    elif next.y != 0:
-      next = Vector2i(2, 0)
-    elif GameData.get_layer(2, 1).unlocked:
-      next = Vector2i(2, 1)
-    else:
-      next = Vector2i(2, 2)
-  SoundController.play_sfx('ui_change', false, -7.0)
-  var next_level:GDLayer = GameData.get_layer(next.x, next.y)
-  if next_level.unlocked:
-    selected = next
+func select(sector:Vector2i):
+  # traverse over all nodes in the sector and pick the closest one
+  var best_node:LevelNode = null
+  var best_dist:float = INF
+  for n:LevelNode in level_nodes:
+    if n == selected_node: continue
+    var a = rad_to_deg(selected_node.pos.angle_to_point(n.pos))
+    if a < -1: a = 360 + a
+    if a >= sector.x && a <= sector.y:
+      var d = selected_node.pos.distance_squared_to(n.pos)
+      if d < best_dist:
+        best_dist = d
+        best_node = n
+  if best_node:
+    SoundController.play_sfx('ui_change', false, -7.0)
+    var next_level:GDLayer = GameData.get_layer(best_node.lay.x, best_node.lay.y)
+    selected = best_node.lay
     selected_level = next_level
+    selected_node = best_node
     level_selected.emit(selected_level)
     queue_redraw()
 
-
 func _draw() -> void:
-  #draw_rect(Rect2(Vector2.ZERO, size), Color.RED)
   var center:Vector2 = size/2
   var show_end:bool = end_debug || !Engine.is_editor_hint() && GameData.get_level(3).unlocked
+  level_nodes = []
+  selected_node = null
   for lvl in range(3):
     var r = remap(lvl, 0, 3, min(size.x, size.y) - layer_radius, 0) / 2.0
     var r_next = remap(lvl + 1, 0, 3, min(size.x, size.y) - layer_radius, 0) / 2.0
@@ -190,12 +211,16 @@ func _draw() -> void:
       if enabled:
         draw_circle(cl, layer_radius, color, true, -1, true)
         draw_circle(cl, layer_radius + arc_width / 2.0, arc_color, false, arc_width, true)
+        var level_node = LevelNode.new(cl, Vector2i(lvl, lay));
+        level_nodes.append(level_node)
+        if selected.x == lvl && selected.y == lay:
+          selected_node = level_node
+          var pulse = sin(Time.get_ticks_msec() / 200.0) * 3.0
+          draw_circle(cl, layer_radius + arc_width + pulse, select_color, false, arc_width + pulse/2, true)
+
       else:
         draw_circle(cl, layer_radius, color.darkened(layer_disabled_dim), true, -1, true)
         draw_circle(cl, layer_radius + ring_width / 2.0, ring_color, false, ring_width, true)
-      if selected.x == lvl && selected.y == lay:
-        var pulse = sin(Time.get_ticks_msec() / 200.0) * 3.0
-        draw_circle(cl, layer_radius + arc_width + pulse, select_color, false, arc_width + pulse/2, true)
 
     if show_end:
       draw_circle(center, layer_radius, arc_color, true, -1, true)
